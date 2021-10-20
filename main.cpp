99af337bad7249487f03e6a550e68166849e53d3
Islam <35372062+Skyt1ess@users.noreply.github.com>
2021-10-20 07:10:26 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,41 +1,90 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
-    string name;
-    name = "Ислам";
-    cout<<"Привет, " + name  << endl;
-    
-    int score = 0, bot_score = 0;
-    bool game = true;
-    while (game == true) {
-        cout << "1 - камень, 2 - ножницы, 3 - бумага, 4 - выход" << endl;
-        int command, bot_command;
-        cin >> command;
-        bot_command = random() % 3 + 1;
-        
-        if (command == 4) {
-            cout << "Игра закончена" << endl;
-            if (score > bot_score) {
-                cout << "Вы победили";
-            }
-            break;
+void printMatrix(vector<vector<char>> v) {
+    cout << "Matrix: \n";
+    for (int i = 0; i < v.size(); i++) {
+        for (int j = 0; j < v[i].size(); j++) {
+            cout << v[i][j];
         }
-        
-        if (command % 3 - bot_command == -1) {
-            score += 1;
-        } else 
-        if (command == bot_command) {
-            score += 1;
-            bot_score += 1;
-        } else {
-            bot_score += 1;
+        cout << endl;
+    }
+}
+
+void readMatrix(vector<vector<char>> &v) {
+    int n = 3, m = 3;
+    //поле 3х3, X - крестик, O - нолик, . - пустое поле
+    cout << "Enter matrix 3x3(X, O or .):\n";
+    v.resize(n);
+    for (int i = 0; i < v.size(); i++) {
+        v[i].resize(m);
+        for (int j = 0; j < v[i].size(); j++) {
+            cin >> v[i][j];
         }
-        cout << "Бот выбрал: " << bot_command << endl; 
-        cout << name + ": " << score << " | Противник: " << bot_score << endl;
-        
     }
 
-    return 0;
+}
+
+string solve(vector<vector<char>> tic_tac_toe) {
+    int countX = 0, countO = 0, n = 3;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (tic_tac_toe[i][j] == 'X') countX++;
+            else if (tic_tac_toe[i][j] == 'O') countO++;
+        }
+    }
+    if (countO > countX || countX - countO > 1) return "Incorrect game";
+
+    bool winX = false, winO = false;
+    vector<int> lineX(3), lineO(3), columnX(3), columnO(3), dX(2), dO(2);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (tic_tac_toe[i][j] == 'X') {
+                lineX[i]++;
+                columnX[j]++;
+                if (i == j) dX[0]++;
+                if (i + j == n - 1) dX[1]++;
+                if (columnX[j] == 3 || lineX[i] == 3 || dX[0] == 3 || dX[1] == 3) winX = true;
+
+            } else
+            if (tic_tac_toe[i][j] == 'O') {
+                lineO[i]++;
+                columnO[j]++;
+                if (i == j) dO[0]++;
+                if (i + j == n - 1) dO[1]++;
+                if (columnO[j] == 3 || lineO[i] == 3 || dO[0] == 3 || dO[1] == 3) winO = true;
+            }
+        }
+    }
+
+    if (winX && winO || winX && (countX == countO) || winO && (countO < countX)) return "Incorrect game";
+    else if (winX) return "Win First";
+    else if (winO) return "win Second";
+
+    if (countX == countO) {
+        if (dX[0] == 2 && dO[0] == 0 || dX[1] == 2 && dO[1] == 0) return "First can win";
+        for (int i = 0; i < n; i++) {
+            if (lineX[i] == 2 && lineO[i] == 0 || columnX[i] == 2 && columnO[i] == 0) return "First can win on next move";
+        }
+    } else {
+        if (dO[0] == 2 && dX[0] == 1|| dO[1] == 2 && dX[1] == 0) return "Second can win";
+        for (int i = 0; i < n; i++) {
+            if (lineO[i] == 2 && lineX[i] == 0 || columnO[i] == 2 && columnX[i] == 0) return "Second can win on next move";
+        }
+    }
+    return "Nobody can't win on next move";
+}
+
+int main() {
+
+    vector<vector<char>> tic_tac_toe;
+    readMatrix(tic_tac_toe);
+
+    cout << solve(tic_tac_toe) << endl;
+
+    printMatrix(tic_tac_toe);
+
 }
